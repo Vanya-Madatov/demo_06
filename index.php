9c4ae1ed56f7e561b9619eda1114caf0d9491b47
Vanya-Madatov <81025053+Vanya-Madatov@users.noreply.github.com>
2022-12-15 19:39:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+
+<body>
+	<form action="send.php" method="post">
+		<input type="text" name="name" placeholder="gdfgdfg" required>
+		<input type="tel" name="phone" placeholder="+34435345345" required>
+		<input type="submit" value="Отправить">
+	</form>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
